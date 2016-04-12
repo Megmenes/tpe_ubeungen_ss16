8f13afc5311cdff55eb79d07cdc2087ea6f850c7
Megmenes <mcskizzy@googlemail.com>
2016-04-12 20:17:00 +0200

    updated BTree and BTreeImpl

--- uebung1_b_baum/src/de/hsmannheim/tpe/ss16/gruppe13/uebung1_b_baum/BTreeImpl.java
+++ uebung1_b_baum/src/de/hsmannheim/tpe/ss16/gruppe13/uebung1_b_baum/BTreeImpl.java
@@ -1,5 +1,83 @@
 package de.hsmannheim.tpe.ss16.gruppe13.uebung1_b_baum;
 
-public class BTreeImpl {
+public class BTreeImpl implements BTree{
+
+	@Override
+	public boolean insert(Integer o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean insert(String filename) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Integer o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int height() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Integer getMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addAll(BTree otherTree) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printInorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printPostorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printPreorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printLevelorder() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
