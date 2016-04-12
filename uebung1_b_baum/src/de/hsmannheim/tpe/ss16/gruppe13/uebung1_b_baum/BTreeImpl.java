c63fda891bfb86c965c6aabdf2802b3f98d55a6a
Megmenes <mcskizzy@googlemail.com>
2016-04-12 20:46:10 +0200

    updated BTreeImpl

--- uebung1_b_baum/src/de/hsmannheim/tpe/ss16/gruppe13/uebung1_b_baum/BTreeImpl.java
+++ uebung1_b_baum/src/de/hsmannheim/tpe/ss16/gruppe13/uebung1_b_baum/BTreeImpl.java
@@ -2,6 +2,39 @@ package de.hsmannheim.tpe.ss16.gruppe13.uebung1_b_baum;
 
 public class BTreeImpl implements BTree{
 
+	private final static int STANDARD_ORDNUNG = 3;
+	private int ordnung;
+	private BTreeNode root;
+	
+	/**
+	 * Constructor <code>BTreeImpl(int ordnung)</code>
+	 * creates a BTree with a ordnung
+	 * @param int ordnung
+	 */
+	public BTreeImpl(int ordnung) {
+		setOrdnung(ordnung);
+	}
+	
+	
+	/* Method <code>void setOrdnung(int ordnung)</code> 
+	 * sets BTree's Ordnung if invalid ordnung (<= 0)
+	 * is given STANDARD_ORDNUNG is set */
+	private void setOrdnung(int ordnung) {
+		if (ordnung <= 0) {
+			this.ordnung = STANDARD_ORDNUNG;
+		} else {
+			this.ordnung = ordnung;
+		}
+	}
+	
+	/** Method <code>int getOrdnung ()</code> returns
+	 * BTree's Ordnung
+	 * @return int BTree's Ordnung
+	 */
+	int getOrdnung() {
+		return this.ordnung;
+	}
+	
 	@Override
 	public boolean insert(Integer o) {
 		// TODO Auto-generated method stub
@@ -44,10 +77,23 @@ public class BTreeImpl implements BTree{
 		return null;
 	}
 
+	/**
+	 * Method <code>boolean isEmpty()</code> returns a
+	 * boolean value depending on if the BTree
+	 * contains NodeElements or not.
+	 * @return boolean true no NodeElements contained/
+	 * false if NodeElements contained
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		//if root == null then Tree is empty
+		//else the Tree has Nodes and therefor
+		//is not empty
+		if(root == null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
